5cdf3ce97425dfb090a0a1ee8f3fcbdcf551267e
Christian Beaudrie <bowtree00@gmail.com>
2016-10-20 02:45:24 +0000

    First commit. Finished dynamic tweets exercise

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,127 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(function() {
+
+// Test / driver code (temporary). Eventually will get this from the server.
+var tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": {
+      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+    },
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1461116232227
+}
+
+var data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+];
+
+
+function renderTweets(tweets) {
+  // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+}
+
+
+function createTweetElement(tweet) {
+
+  var $tweet = $("<article>").addClass("tweet");
+  //$tweet.append("<header>");
+  var avatar = tweet.user.avatars.small;
+  var userName = tweet.user.name;
+  var handle = tweet.user.handle;
+
+   $tweet.append('<header><img class="avatar" src="' + avatar + '"><h2>' + userName + '</h2><div>' + handle + '</div></header>');
+
+   var content = tweet.content.text;
+   var postDate = tweet.created_at;
+   var d = new Date();
+   var currentDate = d.getTime();
+   var daysAgo = Math.floor((d - postDate) / (1000*60*60*24));
+
+   $tweet.append('<div class="body">' + content + '</div>');
+
+   $tweet.append('<footer><div>' + daysAgo + ' days ago</div><div class="footer-icons">');
+
+    // console.log($tweet);
+
+  return $tweet;
+}
+
+
+function renderTweets (tweets_data) {
+
+  tweets = [];
+
+  tweets_data.forEach(function(element) {
+    var $tweet = createTweetElement(element);
+    tweets.push($tweet);
+  });
+
+  return tweets;
+}
+
+
+//$tweet = createTweetElement(tweetData);
+$tweets = renderTweets(data);
+
+// // Test / driver code (temporary)
+// console.log($tweet); // to see what it looks like
+// $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+
+$('#tweets-container').append($tweets); 
+
+})
+
---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,37 @@
+$(function() {
+
+  var maxLength = 140;
+
+  $(".new-tweet textarea").on("keyup", function() {
+
+    var $counter = $(this) // Traverse the DOM and grab the counter
+      .parent("form")
+      .find(".counter"); 
+
+    // var currLength = this.value.length; // NOTE: This way does not require that 'this' is wrapped as a jQuery object, and thus there is less overhead to process this line of code. But we want to be able to use .val() here, so that's what I've kept below
+    
+    var $currLength = $(this).val().length;
+    
+    $counter.text(maxLength - $currLength);
+
+    console.log("$counter.text(): " + $counter.text());
+
+    if ($counter.text() < 0 ) {
+      // $counter.css("color", "red");
+      
+      $counter.addClass("negative");  // changes colour by adding a new class to the element which is styled differently in the CSS file
+
+      console.log("here");
+    } else {
+      // $counter.css("color", "black");
+      $counter.removeClass("negative"); 
+    }
+
+
+  })
+
+
+
+
+
+}) // EOF
